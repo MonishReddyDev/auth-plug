8d2302f0f090b124b035aaae9911d653fcf1ab50
monish <monishreddy9991@gmail.com>
2025-06-12 15:37:08 -0500

    Initial commit: Setup Auth-Plug playground with Express, TypeScript, Prisma

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(cookieParser());
+
+app.get("/ping", (req, res) => {
+  res.json("Auth-Plug is live");
+});
+
+export default app;
